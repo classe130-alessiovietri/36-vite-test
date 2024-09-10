022c9c893075197630e5b8bd349d55d096b04066
Alessio Vietri <info@alessiovietri.it>
2024-09-10 10:49:45 +0200

    Aggiunte immagini

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,18 +3,35 @@ export default {
     data() {
         return {
             text: "Lorem ipsum dolor sit amet",
+            imageName: 'vue.svg'
         };
     },
     methods: {
         clicked() {
             alert("Ciao da clicked");
         },
+        // imageLink: function(img) {
+        imageLink(img) {
+            const url = new URL(`../assets/${img}`, import.meta.url);
+            // console.log(url);
+
+            const link = url.href;
+            // console.log(link);
+
+            return link;
+        }
     },
 };
 </script>
 
 <template>
     <main>
+        <div>
+            <img src="../assets/vue.svg">
+            <img src="/vite.svg">
+            <img :src="imageLink(imageName)">
+        </div>
+
         <p>
             {{ text }}
         </p>
